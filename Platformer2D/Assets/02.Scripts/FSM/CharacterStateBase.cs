363de6124dfaa9b6f0a28125e931054f65c27cc9
1k10 <kyeol1235@gmail.com>
2023-10-18 17:53:54 +0900

    231018 - ETL
    Platformer2D
    - FSM

---
+++ Platformer2D/Assets/02.Scripts/FSM/CharacterStateBase.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using CharacterController = Platformer.Controllers.CharacterController;
+
+namespace Platformer.FSM
+{
+    public enum CharacterStateID
+    {
+        None,
+        Idle,
+        Move,
+        Jump,
+        DownJump,
+        DoubleJump,
+        Fall,
+        Land,
+        Crouch,
+        Hurt,
+        Die,
+        Attack,
+        WallSlide,
+        Edge,
+        EdgeClimb,
+        Ladder,
+    }
+
+    public class CharacterStateBase : IState<CharacterStateID>
+    {
+        public virtual CharacterStateID id { get; }
+
+        public virtual bool canExecute => true;
+
+        protected StateMachine<CharacterStateID> machine;
+        protected CharacterController controller;
+        protected Transform transform;
+        protected Rigidbody2D rigidbody;
+        protected Animator animator;
+
+        public CharacterStateBase(StateMachine<CharacterStateID> machine)
+        {
+            this.machine = machine;
+            this.controller = machine.owner;
+            this.transform = machine.owner.transform;
+            this.rigidbody = machine.owner.GetComponent<Rigidbody2D>();
+            this.animator = machine.owner.GetComponentInChildren<Animator>();
+        }
+
+
+        public virtual void OnStateEnter()
+        {
+
+        }
+
+        public virtual void OnStateExit()
+        {
+
+        }
+
+        public virtual CharacterStateID OnStateUpdate()
+        {
+            return id;
+        }
+
+        public virtual void OnStateFixedUpdate()
+        {
+
+        }
+    }
+}
---
+++ Platformer2D/Assets/02.Scripts/FSM/StateMachineOfT.cs
@@ -0,0 +1,36 @@
+using System;
+using UnityEngine;
+using Platformer.Controllers;
+using CharacterController = Platformer.Controllers.CharacterController;
+using System.Collections.Generic;
+
+namespace Platformer.FSM
+{
+    public class StateMachine<T>
+        where T : Enum
+    {
+        public CharacterController owner;
+        public T currentStateID;
+        protected Dictionary<T, IState<T>> states;
+
+        public void Init(IDictionary<T, IState<T>> copy)
+        {
+            states = new Dictionary<T, IState<T>>(copy);
+        }
+
+        public bool ChangeState(T newStateID)
+        {
+            // 현재 상태와 동일한 상태로 바꿀 필요 없음.
+            if (Comparer<T>.Default.Compare(newStateID, currentStateID) == 0)
+                return false;
+
+            if (states[newStateID].canExecute == false)
+                return false;
+
+            states[currentStateID].OnStateExit();
+            currentStateID = newStateID;
+            states[currentStateID].OnStateEnter();
+            return true;
+        }
+    }
+}
