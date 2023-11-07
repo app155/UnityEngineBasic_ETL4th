8151f0624d72b68c8869ab4f92fad050400e42ac
1k10 <kyeol1235@gmail.com>
2023-11-07 17:51:03 +0900

    231107 - ETL
    CreateMap

---
+++ CSharpBasic/Algorithms/Fibonacci.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    internal class Fibonacci
+    {
+        public int this[int n]
+        {
+            get
+            {
+                if (n < 0)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+
+                if (n > _limit)
+                {
+                    return Get(n);
+                }
+
+                return Cache[n];
+            }
+        }
+
+        public int[] Cache;
+        private int _limit;
+
+        public Fibonacci(int n)
+        {
+            Get(n);
+        }
+
+        public int Get(int n)
+        {
+            _limit = n;
+            Cache = new int[n + 1];
+            return F(n);
+        }
+
+        private int F(int n)
+        {
+            if (n <= 1)
+                return n;
+
+            if (Cache[n] != 0)
+                return Cache[n];
+
+            Cache[n] = F(n - 1) + F(n - 2);
+            return Cache[n];
+        }
+    }
+}
